db35f963e5e3b1f93786bf1a9bb36062de42160c
Mihailo Jovkovic <mihailo.jovkovic@vicert.com>
2012-01-23 14:00:36 +0100

    update 001

--- src/com/vicert/git/test/Main.java
+++ src/com/vicert/git/test/Main.java
@@ -6,7 +6,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World again!!");
+		System.out.println("Hello World again!");
+
+		System.out.println("Bye!");
 	}
 
 }
